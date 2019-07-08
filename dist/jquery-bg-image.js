69ab065750221ec378084a447c76a63857c96b7f
Roberto Godoy <godoyrw@gmail.com>
2019-07-08 17:13:10 -0300

    Refactoring
    Optimize the plugin function

--- dist/jquery-bg-image.js
+++ dist/jquery-bg-image.js
@@ -5,7 +5,6 @@
  @version 1.0.0
  @url https://github.com/godoyrw/jquery-bg-image
  **/
-
 (function ($) {
     /**
      * Explicitly strict mode
@@ -23,31 +22,31 @@
         /**
          * Check if is function callback
          */
-        if (typeof callback !== "function") return false;
+        if (typeof callback !== "function") {
+            console.error("BG Image JS: Callback is not a function.", this);
+            return false;
+        }
 
         var regexp = /url\((.+)\)/i;
         var res = regexp.exec($(this).css("background-image"));
 
         /**
-         * Check if has background image
+         * On error
          */
-        if (res != null) {
-            var img = $("<img id='bgImage' alt='BG Image JS' src='#' />");
-            img.hide();
-            img.bind("load", function (event) {
-                callback.call(this, event);
-                img.remove();
-            });
-            $('body').append(img);
-            img.attr('src', res[1].replace(/["']/gi, ""));
-
-        } else {
-            /**
-             * On error
-             */
-            console.error("BG Image JS: Image background not set", this);
+        if (res === null) {
+            console.error("BG Image JS: Image background not set.", this);
             return false;
         }
+
+        var img = $("<img id='bgImage' alt='BG Image JS' src='#' />");
+        img.hide();
+        img.bind("load", function (event) {
+            callback.call(this, event);
+            img.remove();
+        });
+        $('body').append(img);
+        img.attr('src', res[1].replace(/["']/gi, ""));
+
     }
 
 })(jQuery);
